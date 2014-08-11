2e79f02017b5c47f8e3122f6123321daa490f647
Irene Ros <imirene@gmail.com>
2014-08-11 14:05:40 -0400

    added footer switching

---
+++ src/modules/components/questions.js
@@ -0,0 +1,39 @@
+define(function(require) {
+  var Backbone = require('backbone');
+  var $ = require('jquery');
+
+  var questionTemplates = {
+    versions: require('tmpl!src/modules/components/questions-versions'),
+    age: require('tmpl!src/modules/components/questions-age'),
+    dependents: require('tmpl!src/modules/components/questions-dependents')
+  };
+
+
+  return Backbone.View.extend({
+    template : questionTemplates.versions,
+    events: {
+      'click li a' : 'questionClick'
+    },
+
+    initialize: function() {
+
+      // when someone tells us to update the questions, change to the
+      // appropriate type
+      this.on('change-questions', this.questionChange);
+    },
+
+    questionChange: function(type) {
+      this.$el.html(questionTemplates[type]());
+    },
+
+    questionClick: function(ev) {
+      ev.stopPropagation();
+      ev.preventDefault();
+      var target = $(ev.target);
+      var q = target.attr('class');
+      this.trigger('question-switch', q);
+      console.log(q);
+      return false;
+    }
+  });
+});
--- src/modules/core/footer.js
+++ src/modules/core/footer.js
@@ -1,8 +1,48 @@
 define(function(require) {
   var Backbone = require('backbone');
+  var $ = require('jquery');
+  var Questions = require('src/modules/components/questions');
 
   return Backbone.View.extend({
-    template : require('tmpl!src/modules/templates/footer-start')
+    template : require('tmpl!src/modules/templates/footer-start'),
+
+    events: {
+      'click a.start': 'onStart',
+      'click .grid-menu li': 'gridSwitch'
+    },
+
+    initialize: function() {
+      this.questionsView = null;
+    },
+
+    onStart: function(ev) {
+      ev.preventDefault();
+      ev.stopPropagation();
+
+      // replace footer with controls footer
+      this.$el.html(require('tmpl!src/modules/templates/footer-controls')());
+
+      // render default
+      this.questionsView = new Questions();
+      this.insertView('.questions ul', this.questionsView).render();
+
+      return false;
+    },
+
+    gridSwitch: function(ev) {
+
+      // remove selected from current
+      var target = $(ev.target);
+      target.parent().find('.selected').removeClass('selected');
+      target.addClass('selected');
+
+      // tell the questions view to update its grid
+      var questionType = target.data('grid');
+      this.questionsView.trigger('change-questions', questionType);
+
+      return false;
+    }
+
   });
 
 });
