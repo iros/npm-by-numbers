90cca8bcdb49860c39aa5ade0960cdfbf344a141
Irene Ros <imirene@gmail.com>
2014-08-20 14:02:41 -0400

    fixed push state url overwrite in grunt

--- build/contrib-connect.js
+++ build/contrib-connect.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
   var root = '/index.html';
   function rewriteRoute(req, res, next) {
     if (/^\/(?:breakdown\/[^\/]+)$/.test(req.url) ||
-        /^\/(?:breakdown\/[A-Za-z+\/]+)$/.test(req.url)) {
+        /^\/(?:breakdown\/[A-Za-z+\/\_0-9]+)$/.test(req.url)) {
       grunt.log.debug('PUSHSTATE ' + req.url + ' -> ' + root);
       req.url = root;
     }
