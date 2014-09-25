5831c99f8b209d693e4741a3c576466c2fe4b057
Irene Ros <imirene@gmail.com>
2014-09-25 15:49:25 -0400

    added amazon deploy

---
+++ build/s3.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt) {
+
+  var creds = grunt.file.readJSON("credentials.json");
+
+  if (typeof creds === "undefined") {
+    throw new Error("Setup your credentials.json file from the sample!");
+  }
+
+  grunt.config.set('s3', {
+
+    options: {
+      key: creds.accessKeyId,
+      secret: creds.secretAccessKey,
+      bucket: "npmvis.bocoup.com",
+      access: 'public-read',
+      cache: false,
+      headers: {
+
+        // expire in an hour
+        "Cache-Control": "max-age=3600000, public",
+        "Expires": new Date(Date.now() + 3600000).toUTCString()
+      }
+    },
+    deploy: {
+      upload: [
+        {
+          src: "prod/*.{html,txt,png,xml,ico,css,js,js.map}",
+          dest: "/",
+          rel: "prod"
+        },
+        {
+          src: "prod/data/*",
+          dest: "/",
+          rel: "prod"
+        },
+        {
+          src: "prod/img/*",
+          dest: "/",
+          rel: "prod"
+        },
+      ]
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-s3');
+};
